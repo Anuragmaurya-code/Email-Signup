b0f334cbbbe6e493ff2f6dae8f95573f7c5e763d
Anurag Maurya <mauryaanurag375@gmail.com>
2022-12-24 15:26:18 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const https = require("https");
+const app = express();
+const client = require("@mailchimp/mailchimp_marketing");
+client.setConfig({
+  apiKey: "e63f95080fc53fc02abc084a97def6fb-us21",
+  server: "us21",
+});
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstname = req.body.firstname;
+  const lastname = req.body.lastname;
+  const email = req.body.email;
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("e9a92fdabf", {
+        email_address: email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: firstname,
+          LNAME: lastname
+        },
+      });
+      console.log(response);
+      res.sendFile(__dirname+"/success.html");
+    } catch (e) {
+      console.log(e);
+      res.sendFile(__dirname+"/failure.html");
+    }
+  };
+  run();
+  //   if(response.status==='subscribed')
+  //   res.send("OK");
+  //   else
+  //   res.send("Not ok");
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server connected");
+});
+
+// Api key = e63f95080fc53fc02abc084a97def6fb-us21
+
+// audience id = e9a92fdabf
