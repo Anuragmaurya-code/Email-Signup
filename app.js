79c6ecc8d05d7fcd653aa0aeb2021cc6bae7f127
Anurag Maurya <mauryaanurag375@gmail.com>
2022-12-24 16:21:31 +0530

    update

--- app.js
+++ app.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser');
 const https = require("https");
 const app = express();
 const client = require("@mailchimp/mailchimp_marketing");
+require('dotenv').config()
+const APIKEYVALUE=process.env.APIKEYVALUE;
+const SERVERVALUE=process.env.SERVERVALUE;
+const LISTKEY=process.env.LISTKEY;
 client.setConfig({
-  apiKey: "e63f95080fc53fc02abc084a97def6fb-us21",
-  server: "us21",
+  apiKey: APIKEYVALUE,
+  server: SERVERVALUE,
 });
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({
@@ -22,7 +26,7 @@ app.post("/", function(req, res) {
   const email = req.body.email;
   const run = async () => {
     try {
-      const response = await client.lists.addListMember("e9a92fdabf", {
+      const response = await client.lists.addListMember(LISTKEY, {
         email_address: email,
         status: "subscribed",
         merge_fields: {
